b35b6fabda0a1580d2be95e51896b74d595dc990
EZEQUIEL FELICIANO DOS SANTOS <ezequiel.fsantos10@senacsp.edu.br>
2024-09-25 15:11:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// ota do aluno
+
+let nota = parseFloat(prompt("Digite a nota"))
+
+// Verifificar se o aluno foi aprovado
+
+if (nota >= 7) {
+     alert("Aprovado");
+}else {
+    alert("Reprovado");
+}
